7743570351777a07779ef48a4bf5a88c3ff59115
Emmanuel Ariza <emmanuel@emmanuel.emmanuel-pc>
2017-12-30 17:23:37 -0500

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name emptyChairWidgetApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the emptyChairWidgetApp
+ */
+angular.module('emptyChairWidgetApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
