c7123c999f14e54fd54ee10231ef910fdde49d0e
Emmanuel Ariza <emmanuel@emmanuel.emmanuel-pc>
2017-12-30 18:54:41 -0500

    Updates

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name emptyChairWidgetApp
+ * @description
+ * # emptyChairWidgetApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('emptyChairWidgetApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'nvd3',
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
