58833c9b0e3c33af44559f050b08dcd11d504f45
Emmanuel Ariza <emmanuel@emmanuel.emmanuel-pc>
2018-01-15 00:16:58 -0500

    Created Graph in Vargas Lleras Graph

--- app/scripts/controllers/wg-vargas-lleras-discurso.js
+++ app/scripts/controllers/wg-vargas-lleras-discurso.js
@@ -16,22 +16,84 @@ angular.module('emptyChairWidgetApp')
       'Karma'
     ];
 
-    //$scope.data = Restangular.one("vargas-lleras-words").get();
+    $scope.data = data;
     $scope.selectedWords = [];
     $scope.words = Object.keys(data.words)
     .map(function(element, index){
       return {word:element, category:index%4} ;
     });
-/*    $scope.words = [
-    				{word:"alpha", category:"a"}, 
-    				{word:"alpha8", category:"a"}, 
-    				{word:"beta", category:"b"},
-    				{word:"betacaroteno", category:"b"},
-    				{word:"gamma", category:"c"},
-    				{word:"delta", category:"c"},
-    				{word:"epsilon", category:"d"},
-    				{word:"omega", category:"d"},
-    				{word:"omega10", category:"d"},
+    $scope.data3 = $scope.words.map(function(wordData) {
+         var element = data.words[wordData.word]
+         return {values:element.norm1.map(function(value, index){
+                    return {x:index, y:value};
+                }),
+                 key   :wordData.word}
+                }).slice(1, 2);
 
-    				];*/
+    $scope.$watchCollection('selectedWords', function(){
+      console.log("called!!!!!!!!!! Ashes to ashes");
+      $scope.data3 = $scope.selectedWords.map(function(wordData) {
+           var element = data.words[wordData.word]
+           return {values:element.norm1.map(function(value, index){
+                      return {x:index, y:value};
+                  }),
+                   key   :wordData.word}
+                  });
+    })
+
+    $scope.options = {
+            chart: {
+                type: 'lineChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 40,
+                    left: 55
+                },
+                x: function(d){ return d.x; },
+                y: function(d){ return d.y; },
+                useInteractiveGuideline: true,
+                dispatch: {
+                    stateChange: function(e){ console.log("stateChange"); },
+                    changeState: function(e){ console.log("changeState"); },
+                    tooltipShow: function(e){ console.log("tooltipShow"); },
+                    tooltipHide: function(e){ console.log("tooltipHide"); }
+                },
+                xAxis: {
+                    axisLabel: 'Time (ms)'
+                },
+                yAxis: {
+                    axisLabel: 'Voltage (v)',
+                    tickFormat: function(d){
+                        return d3.format('.02f')(d);
+                    },
+                    axisLabelDistance: -10
+                },
+                callback: function(chart){
+                    console.log("!!! lineChart callback !!!");
+                }
+            },
+            title: {
+                enable: true,
+                text: 'Title for Line Chart'
+            },
+            subtitle: {
+                enable: true,
+                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+                css: {
+                    'text-align': 'center',
+                    'margin': '10px 13px 0px 7px'
+                }
+            },
+            caption: {
+                enable: true,
+                html: '<b>Figure 1.</b> Lorem ipsum.',
+                css: {
+                    'text-align': 'justify',
+                    'margin': '10px 13px 0px 7px'
+                }
+            }
+        };
+
   }]);
