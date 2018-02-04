4d9b743b3a308008eb1478106143fef675ed74f3
Emmanuel Ariza <emmanuel@emmanuel.emmanuel-pc>
2018-02-03 23:51:36 -0500

    Created Talkviz controller

---
+++ app/scripts/controllers/talkviz.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name emptyChairWidgetApp.controller:TalkvizCtrl
+ * @description
+ * # TalkvizCtrl
+ * Controller of the emptyChairWidgetApp
+ */
+angular.module('emptyChairWidgetApp')
+  .controller('TalkvizCtrl', ["$scope", function ($scope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.htmlTooltipGenerator = function(e){
+                  var series = e.series[0];
+                  var point = e.point;
+                  if (series.value === null) return;
+                  
+                  var rows = 
+                    "<tr>" +
+                      "<td class='key'>" + 'Time: ' + "</td>" +
+                      "<td class='x-value'>" + e.value + "</td>" + 
+                    "</tr>" +
+                    "<tr>" +
+                      "<td class='key'>" + 'Voltage: ' + "</td>" +
+                      "<td class='x-value'><strong>" + (series.value?series.value.toFixed(2):0) + "</strong></td>" +
+                    "</tr>";
+
+                  var header = 
+                    "<thead>" + 
+                      "<tr>" +
+                        "<td class='legend-color-guide'><div style='background-color: " + series.color + ";'></div></td>" +
+                        "<td class='key'><strong>" + point.word + "</strong></td>" +
+                      "</tr>" + 
+                    "</thead>";
+                    
+                  return "<table>" +
+                      header +
+                      "<tbody>" + 
+                        rows + 
+                      "</tbody>" +
+                    "</table>";    	
+    }
+  }]);
---
+++ test/spec/controllers/talkviz.js
@@ -0,0 +1,55 @@
+'use strict';
+
+describe('Controller: TalkvizCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('emptyChairWidgetApp'));
+
+  var TalkvizCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    TalkvizCtrl = $controller('TalkvizCtrl', {
+      $scope: scope
+      // place here mocked dependencies
+    });
+  }));
+
+  xit('should attach a list of awesomeThings to the scope', function () {
+    expect(TalkvizCtrl.awesomeThings.length).toBe(3);
+  });
+
+  it('Should return html for tooltip when htmlTooltipGenerator is called',function(){
+    var elementMock = {
+      value: 5,
+      series:[
+        {value: Math.PI,
+         color: "red"
+        }],
+      point:{
+        word:"some-word"
+      }
+    }
+    var expectedHtml = "<table>" +
+                       "<thead>" + 
+                      "<tr>" +
+                        "<td class='legend-color-guide'><div style='background-color: red;'></div></td>" +
+                        "<td class='key'><strong>some-word</strong></td>" +
+                      "</tr>" + 
+                    "</thead>" +
+                      "<tbody>" + 
+                    "<tr>" +
+                      "<td class='key'>" + 'Time: ' + "</td>" +
+                      "<td class='x-value'>5</td>" + 
+                    "</tr>" +
+                    "<tr>" +
+                      "<td class='key'>" + 'Voltage: ' + "</td>" +
+                      "<td class='x-value'><strong>3.14</strong></td>" +
+                    "</tr>" + 
+                      "</tbody>" +
+                    "</table>"
+    expect(scope.htmlTooltipGenerator(elementMock)).toEqual(expectedHtml);
+  })
+});
